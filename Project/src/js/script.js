628cab7bdfab56eee003caa466969e49ad31d7c7
Наврузов <167851240+mohmadnavruzov084@users.noreply.github.com>
2024-10-03 16:37:05 +0300

    Add files via upload

---
+++ Project/src/js/script.js
@@ -0,0 +1,14 @@
+let goBtn=document.querySelector(".go_Btn");
+let img=document.querySelector(".image")
+goBtn.onclick=function(){
+    img.style.display="none";
+    if(img.style.display="none"){
+        goBtn.onclick=function(){ 
+            img.style.display="block"
+        }
+    }
+    else{
+      
+    }
+   
+}
